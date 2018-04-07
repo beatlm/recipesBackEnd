70502bf6e9aea7a76ca4f7ac3d53b2063940d7b3
Beatriz Lopez <beatlm@gmail.com>
2018-04-07 16:34:57 +0200

    incluir id en la salida

---
+++ src/main/java/com/thermomix/recipes/repository/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package com.thermomix.recipes.repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import com.thermomix.recipes.entity.Recipe;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+    @Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    	config.exposeIdsFor(Recipe.class);
+    }
+}
