6de9727972a721bfd03c9d4e47d9a9214a9f82fd
Beatriz Lopez <beatlm@gmail.com>
2018-04-07 16:29:19 +0200

    cors delete

--- src/main/java/com/thermomix/recipes/repository/RecipeRepository.java
+++ src/main/java/com/thermomix/recipes/repository/RecipeRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.thermomix.recipes.entity.Recipe;
 
 @RepositoryRestResource(collectionResourceRel = "result", path = "recipes") 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", methods={RequestMethod.GET, RequestMethod.DELETE})
 public interface RecipeRepository extends PagingAndSortingRepository<Recipe,String> {
 	
 	@RestResource( rel="buscarPorNumeroCuenta", path="findByName" )
