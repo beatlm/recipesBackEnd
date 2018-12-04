f6b64854560b263fe171f195874bcfcf71b2470e
blopezmo <blopezmo@vectoritcgroup.com>
2018-12-04 13:25:22 +0100

    add allowedOrigin in filterRegistrationBean

--- src/main/java/com/thermomix/recipes/RecipesApplication.java
+++ src/main/java/com/thermomix/recipes/RecipesApplication.java
@@ -2,6 +2,11 @@ package com.thermomix.recipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class RecipesApplication {
@@ -9,6 +14,23 @@ public class RecipesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipesApplication.class, args);
 	}
+	
+	@Bean
+	  public FilterRegistrationBean<CorsFilter> corsFilter() {
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("https://tm-recipes-front.herokuapp.com"); 
+	    config.addAllowedOrigin("http://localhost:4200");
+	    config.addAllowedOrigin("http://localhost:8080");
+	    //config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", config);
+	    final FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	    bean.setOrder(0);
+	    return bean;
+	  }
 	
 	
 }
