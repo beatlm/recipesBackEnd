4480ea77abbac71bbb35d29059e61ef063f30303
blopezmo <blopezmo@vectoritcgroup.com>
2018-12-26 10:52:48 +0100

    Añadido @RepositoryRestResource

--- src/main/java/com/thermomix/recipes/repository/CustomRecipeRepository.java
+++ src/main/java/com/thermomix/recipes/repository/CustomRecipeRepository.java
@@ -2,10 +2,12 @@ package com.thermomix.recipes.repository;
 
 import java.util.List;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.thermomix.recipes.entity.Recipe;
 
+@RepositoryRestResource(collectionResourceRel = "result", path = "recipes") 
 //@CrossOrigin(origins = {"http://localhost:4200", "https://tm-recipes-front.herokuapp.com"}, 
 //methods={RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PATCH})
 public interface CustomRecipeRepository {
