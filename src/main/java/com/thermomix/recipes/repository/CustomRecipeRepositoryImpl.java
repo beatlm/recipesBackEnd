c4661447e0b7b9d79f0d5e2bbf567d3ac2763df4
blopezmo <blopezmo@vectoritcgroup.com>
2019-01-03 13:08:16 +0100

    added database uri

--- src/main/java/com/thermomix/recipes/repository/CustomRecipeRepositoryImpl.java
+++ src/main/java/com/thermomix/recipes/repository/CustomRecipeRepositoryImpl.java
@@ -16,15 +16,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public  class CustomRecipeRepositoryImpl implements CustomRecipeRepository {
 @Value("${spring.data.mongodb.database}")
-private String database;
+private String databaseName;
+@Value("${spring.data.mongodb.uri}")
+private String databaseUri;
 	@Override
 	public List<Recipe>buscarPorTag(String tag){
 		log.info("Se busca el tag "+tag);
 		//AnnotationConfigApplicationContext  ctx = new AnnotationConfigApplicationContext();//SpringMongoConfig.class);
 		log.info("1 ");
 		//ctx.refresh();
-		log.info("1.5 ");
-		MongoOperations mongoOperations = new MongoTemplate(new MongoClient(), database);;//(MongoOperations) ctx.getBean("mongoTemplate");
+		log.info("1.5 "+databaseName+" - "+databaseUri);
+		MongoClient client=new MongoClient(databaseUri);
+		MongoOperations mongoOperations = new MongoTemplate(client, databaseName);;//(MongoOperations) ctx.getBean("mongoTemplate");
 		log.info("2");
 		Query q = new Query(Criteria.where("tags").all(tag));
 		log.info("3");
