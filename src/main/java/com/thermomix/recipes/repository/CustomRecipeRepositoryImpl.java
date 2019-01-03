45c58c93d05251aba510c8e74bac17a116014b65
blopezmo <blopezmo@vectoritcgroup.com>
2019-01-03 13:00:33 +0100

    modificamos conexion mongodb para el criteria

--- src/main/java/com/thermomix/recipes/repository/CustomRecipeRepositoryImpl.java
+++ src/main/java/com/thermomix/recipes/repository/CustomRecipeRepositoryImpl.java
@@ -2,25 +2,29 @@ package com.thermomix.recipes.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import com.mongodb.MongoClient;
 import com.thermomix.recipes.entity.Recipe;
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public  class CustomRecipeRepositoryImpl implements CustomRecipeRepository {
-
+@Value("${spring.data.mongodb.database}")
+private String database;
 	@Override
 	public List<Recipe>buscarPorTag(String tag){
 		log.info("Se busca el tag "+tag);
-		AnnotationConfigApplicationContext  ctx = new AnnotationConfigApplicationContext();//SpringMongoConfig.class);
+		//AnnotationConfigApplicationContext  ctx = new AnnotationConfigApplicationContext();//SpringMongoConfig.class);
 		log.info("1 ");
-		ctx.refresh();
+		//ctx.refresh();
 		log.info("1.5 ");
-		MongoOperations mongoOperations = (MongoOperations) ctx.getBean("mongoTemplate");
+		MongoOperations mongoOperations = new MongoTemplate(new MongoClient(), database);;//(MongoOperations) ctx.getBean("mongoTemplate");
 		log.info("2");
 		Query q = new Query(Criteria.where("tags").all(tag));
 		log.info("3");
