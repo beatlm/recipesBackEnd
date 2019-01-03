8d5cdbcbad6facb6e42835bbdb6d281eea530b14
blopezmo <blopezmo@vectoritcgroup.com>
2019-01-03 12:30:34 +0100

    añadido override

--- src/main/java/com/thermomix/recipes/repository/CustomRecipeRepositoryImpl.java
+++ src/main/java/com/thermomix/recipes/repository/CustomRecipeRepositoryImpl.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public  class CustomRecipeRepositoryImpl implements CustomRecipeRepository {
 
+	@Override
 	public List<Recipe>buscarPorTag(String tag){
 		log.info("Se busca el tag "+tag);
 		ApplicationContext ctx = new AnnotationConfigApplicationContext();//SpringMongoConfig.class);
